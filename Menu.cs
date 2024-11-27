9c4540ba7ad17d7024ebdfcbdce74ddcfd2f718f
Carlos barbosa <carloslk18@gmail.com>
2024-11-26 23:22:23 -0300

    First commit

---
+++ Menu.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace HtmlEditor{
+
+public class Menu{
+
+    public static void Show(){
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.ForegroundColor = ConsoleColor.White;
+
+        DrawScreen();
+    }
+
+    public static void DrawScreen(){
+
+        Console.Write("+");
+        for (int i = 0; i<=30; i++){
+            Console.Write("-");
+        }
+        Console.Write("+");
+        Console.Write("\n");
+
+        for (int lines = 0; lines <= 10; lines++){
+            Console.Write("|");
+            for (int i = 0; i<=30; i++){
+                Console.Write(" ");
+            }
+            Console.Write("|");
+            Console.Write("\n");
+        }
+
+        Console.Write("+");
+        for (int i = 0; i<=30; i++){
+            Console.Write("-");
+        }
+        Console.Write("+");
+        Console.Write("\n");
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HtmlEditor{
+
+class Program{
+
+    public static void Main(string[] args){
+        Menu.Show();
+
+    }
+}
+}
